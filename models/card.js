c490e324974f0158b25aedf0f0882afb141ced4d
dim2a <buinicki.dim@gmail.com>
2019-08-01 12:47:58 +0300

    added opportunity to remove from card

--- models/card.js
+++ models/card.js
@@ -49,6 +49,32 @@ class Card {
       });
     });
   }
+
+  static async remove(id) {
+    const card = await Card.fetch();
+
+    const idx = card.courses.findIndex(c => c.id === id);
+    const course = card.courses[idx];
+
+    if (course.count === 1) {
+      //delete
+      card.courses = card.courses.filter(c => c.id !== id);
+    } else {
+      //change quantity
+      card.courses[idx].count--;
+    }
+    card.price -= course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(card);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Card;
